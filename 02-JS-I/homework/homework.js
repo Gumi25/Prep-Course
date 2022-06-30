61953ddd18e257612f80ab294e4799489488632a
Heriberto Fernandez <hfernandezaguirre@gmail.com>
2022-06-29 23:09:10 -0500

    terminado los 35 ejercicios JS_I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -243,7 +243,7 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
 if (letra.length < 1 )
-  return "Coloca una vocal";
+  return "Coloca una vocal porfavor";
 if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u")
   return "Es vocal";
 else 
